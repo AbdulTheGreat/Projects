85785f7002a3012b58fd848f4f9c107b7198b34d
Abdulrazzaq <144608915+AbdulTheGreat@users.noreply.github.com>
2024-10-13 21:57:44 -0700

    Add files via upload
    Test Website

---
+++ Contact Cards/src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Gaius Julius Caesar",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Retrato_de_Julio_C%C3%A9sar_%2826724093101%29_%28cropped%29.jpg/1280px-Retrato_de_Julio_C%C3%A9sar_%2826724093101%29_%28cropped%29.jpg",
+    phone: "+123 456 789",
+    email: "G@JCaesar.com",
+  },
+  {
+    name: "Mona Lisa",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1024px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg",
+    phone: "+987 654 321",
+    email: "Mlisak@nowhere.com",
+  },
+  {
+    name: "Pontius Pilate",
+    imgURL:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQw5Ar45aFlTxt-Ct_KPTfl4A5n-KQZ4Vt28p1BiX69tCdCdmKH",
+    phone: "+918 372 574",
+    email: "gmail@PontiusPilate.com",
+  },
+];
+export default contacts;
